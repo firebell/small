61497828547afe7ce7128f243aa63402ab3b3f7b
firebell <cerber59@ya.ru>
2014-11-02 23:01:20 +0500

    full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,10 +5,20 @@ describe "StaticPages" do
   let (:base_title) { "My sample app" }
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+
+    it "should have the content 'sample app'" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-      expect(page).to have_content('my best home page')
+      expect(page).to have_content('sample app')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("My sample app")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
