26c4b56214e3daf3151a7267e1da6eabf57aa082
firebell <cerber59@ya.ru>
2014-11-02 02:14:04 -0500

    static pages with test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,24 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_title('Home')
+      expect(page).to have_content('my best home page')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      expect(page).to have_content('help page')
+      expect(page).to have_title('Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+      expect(page).to have_content('about us')
+      expect(page).to have_title('About Us')
     end
   end
 
