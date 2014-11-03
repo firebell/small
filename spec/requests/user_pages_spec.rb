a1c959c3b400508e9705d9e7b14576e19f9e22d9
firebell <cerber59@ya.ru>
2014-11-03 20:24:35 +0500

    tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,16 @@ require 'support/utilities.rb'
 describe "UserPages" do
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
+    let(:heading)    { 'Sign Up' }
+    let(:page_title) { 'Sign Up' }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it_should_behave_like "all static pages"
   end
 end
