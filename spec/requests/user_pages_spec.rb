d21a050c91ee9663d954524183be6ec3e875e351
firebell <cerber59@ya.ru>
2014-11-04 11:40:43 +0500

    tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,6 +26,28 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
+      describe "after submission without confirmation" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+        end
+
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+          expect { should have_title('Sign up') }
+          expect { should have_content('form contains') }
+          expect { should have_content("confirmation doesn't match") }
+        end
+      end
     end
 
     describe "with valid information" do
@@ -38,6 +60,14 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
